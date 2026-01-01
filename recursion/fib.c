1a75ec5cd3f5683e6aa54f6576657d2849a4c6c4
Alessio Arsuffi <alessio.arsuffi@ovolab.com>
2026-01-01 19:53:28 +0100

    add fibonacci in C, C++ and Rust + benchmarks

---
+++ recursion/fib.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+
+uint64_t fib_iterative(int n) {
+    if (n <= 1) return n;
+    
+    uint64_t a = 0, b = 1, temp;
+    for (int i = 2; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
+
+int main() {
+    fib_iterative(30);
+    return 0;
+}
---
+++ recursion/fib.rs
@@ -0,0 +1,21 @@
+fn fib_iterative(n: i32) -> u64 {
+    if n <= 1 {
+        return n as u64;
+    }
+    
+    let mut a: u64 = 0;
+    let mut b: u64 = 1;
+    let mut temp: u64;
+    
+    for _i in 2..=n {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    
+    b
+}
+
+fn main() {
+    fib_iterative(30);
+}
